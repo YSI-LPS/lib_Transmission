596b92e0a48c606da27aaeb093558df5d5ac18a9
YSI <yannic.simon@cnrs.fr>
2026-06-10 16:37:32 +0200

    BIG Update lib_Transmission.h
    There are a lot of changes in this version.
    These include bug fixes and the switch to multi-client support for TCP connections.

--- lib_Transmission.h
+++ lib_Transmission.h
@@ -1,60 +1,80 @@
 /** Transmission class
-*
-* @purpose       library for Transmission
-*
-* Use to Transmission
-*
-* Example:
-* @code
-* #include "lib_Transmission.h"
-* 
-* #define MBED_PROJECT      "Transmission"
-* #define SERIAL_BAUDRATE   230400
-* 
-* string      processing(string);
-* 
-* struct {
-*             const string        idn;
-*             UnbufferedSerial    *ss;
-*             USBCDC              *usb;
-*             EthernetInterface   *eth;
-*             Transmission        *tr;
-* } uc = {
-*             MBED_PROJECT + ((string)" Rev. ") + MBED_STRINGIFY(TARGET_NAME) + " " + __DATE__ + " " + __TIME__ + " MbedOS " + to_string(MBED_VERSION),
-*             #if MBED_MAJOR_VERSION > 5
-*             new UnbufferedSerial(USBTX, USBRX, SERIAL_BAUDRATE),
-*             #else
-*             new Serial(USBTX, USBRX, SERIAL_BAUDRATE),
-*             #endif
-*             new USBCDC(false),
-*             new EthernetInterface,
-*             new Transmission(uc.ss, uc.usb, uc.eth, &processing)
-* };
-* 
-* int main(void)
-* {
-*     uc.tr->ip(true, "192.168.1.25");
-*     while(1) uc.tr->recv();
-* }
-* 
-* string processing(string cmd)
-* {
-*     ostringstream ssend;
-*     ssend << fixed;
-*     ssend.precision(2);
-*     if(cmd.empty());
-*     else if(cmd == "*IDN?\n")
-*         return uc.idn;
-*     else if(cmd == "MAC?\n")
-*         ssend << "MAC\t[" << (uc.eth->get_mac_address()?uc.eth->get_mac_address():"00:00:00:00:00:00") << "]";
-*     else if(cmd.find("?") != string::npos)
-*         ssend << "incorrect requeste [" << cmd << "]";
-*     return ssend.str();
-* }
-* @endcode
-* @file          lib_Transmission.h 
-* @date          Jun 2020
-* @author        Yannic Simon
+
+@purpose       library for Transmission
+
+Use to Transmission
+
+Example:
+@code
+#include "lib_Transmission.h"
+ 
+struct uc_t {
+    static constexpr int SERIAL_BAUDRATE = 230400;
+    const string PROJECT;
+    const string idn;
+    UnbufferedSerial *ss;
+    USBCDC *usb;
+    EthernetInterface *eth;
+    Transmission *tr;
+    int color[8];
+    BusOut *leds;
+    const Transmission::tr_status_t init;
+    Transmission::tr_status_t state;
+    
+    static string fn_processing(string cmd, Transmission::_clientTCP_t *client=nullptr);
+    static void fn_ethup(void);
+    uc_t():
+        PROJECT("SensorIP"),
+        idn(PROJECT + ((string)" Rev. ") + MBED_STRINGIFY(TARGET_NAME) + " " + __DATE__ + " " + __TIME__ + " MbedOS " + to_string(MBED_VERSION)),
+        ss(new UnbufferedSerial(USBTX, USBRX, SERIAL_BAUDRATE)),
+        usb(new USBCDC(false)),
+        eth(new EthernetInterface),
+        tr(new Transmission(ss, usb, eth, &fn_processing, &fn_ethup)),
+        #if (defined(TARGET_LPC1768) || defined(TARGET_K64F))
+        color{ 0, 1, 2, 3, 4, 5, 6, 7 },
+        #elif (defined(TARGET_NUCLEO_H743ZI2) || defined(TARGET_NUCLEO_F767ZI) || defined(TARGET_NUCLEO_F429ZI)|| defined(TARGET_NUCLEO_F746ZG)|| defined(TARGET_NUCLEO_F756ZG))
+        color{ 7, 6, 3, 2, 5, 1, 4, 0 },
+        #else
+        #error color not defined for this target
+        #endif
+        leds(new BusOut(LED1, LED2, LED3)),
+        init(Transmission::WHITE_STATUS),
+        state(Transmission::RED_DISCONNECTED)
+    {}
+} uc;
+ 
+int main(void)
+{
+    uc.leds->write(uc.color[uc.init]);
+    ThisThread::sleep_for(500ms);
+    uc.leds->write(uc.color[uc.state]);
+    uc.tr->ip(true);//, "192.168.1.25");
+    while(true) uc.leds->write(uc.color[uc.state = uc.tr->recv()]);
+}
+
+void uc_t::fn_ethup()
+{
+    printf("ETHUP\n");
+}
+
+string uc_t::fn_processing(string cmd, Transmission::_clientTCP_t *client)
+{
+    ostringstream ssend;
+    ssend << fixed;
+    ssend.precision(2);
+    if(cmd.empty());
+    else if(cmd == "*IDN?\n")
+        return uc.idn;
+    else if(cmd == "MAC?\n")
+        ssend << "MAC\t[" << (uc.eth->get_mac_address()?uc.eth->get_mac_address():"00:00:00:00:00:00") << "]";
+    else if(cmd.find("?") != string::npos)
+        ssend << "?";
+    return ssend.str();
+}
+@endcode
+@file          lib_Transmission.h 
+@date          Jun 2020
+@author        Yannic Simon
 */
 #ifndef TRANSMISSION_H
 #define TRANSMISSION_H
@@ -76,16 +96,113 @@ class Transmission
     public:
         /** enumerator of the different possible delivery of the transmission library
         */
-        typedef enum { USB_DELIVERY, SERIAL_DELIVERY, TCP_DELIVERY, HTTP_DELIVERY, ANY_DELIVERY }
-            enum_trans_delivery;
+        enum tr_delivery_t {
+            USB_DELIVERY,
+            SERIAL_DELIVERY,
+            TCP_DELIVERY,
+            HTTP_DELIVERY,
+            ANY_DELIVERY
+        };
         /** enumerator of the different ethernet connexion status of the transmission library
         */
-        typedef enum { WHITE_STATUS, CYAN_STATUS, MAGENTA_ACCEPT, BLUE_CLIENT, YELLOW_CONNECTING, GREEN_GLOBAL_UP, RED_DISCONNECTED, BLACK_INITIALIZE }
-            enum_trans_status;
+        enum tr_status_t {
+            WHITE_STATUS,
+            CYAN_STATUS,
+            MAGENTA_ACCEPT,
+            BLUE_CLIENT,
+            YELLOW_CONNECTING,
+            GREEN_GLOBAL_UP,
+            RED_DISCONNECTED,
+            BLACK_INITIALIZE
+        };
         /** List of http headers returns
         */
-        struct { const char *RETURN_OK; const char *RETURN_NO_CONTENT; const char *RETURN_RESET_CONTENT; const char *RETURN_MOVED; const char *RETURN_FOUND; const char *RETURN_SEE_OTHER; const char *RETURN_REDIRECT; const char *RETURN_NOT_FOUND; }
-            http = { "HTTP/1.1 200 OK\r\n", "HTTP/1.1 204 No Content\r\n", "HTTP/1.1 205 Reset Content\r\n", "HTTP/1.1 301 Moved Permanently\r\n", "HTTP/1.1 302 Found\r\n", "HTTP/1.1 303 See Other\r\n", "HTTP/1.1 307 Temporary Redirect\r\n", "HTTP/1.1 404 Not Found\r\n" };
+        struct _returnHTTP_t {
+            const char *RETURN_OK = "HTTP/1.1 200 OK\r\n";
+            const char *RETURN_NO_CONTENT = "HTTP/1.1 204 No Content\r\n";
+            const char *RETURN_RESET_CONTENT = "HTTP/1.1 205 Reset Content\r\n";
+            const char *RETURN_MOVED = "HTTP/1.1 301 Moved Permanently\r\n";
+            const char *RETURN_FOUND = "HTTP/1.1 302 Found\r\n";
+            const char *RETURN_SEE_OTHER = "HTTP/1.1 303 See Other\r\n";
+            const char *RETURN_REDIRECT = "HTTP/1.1 307 Temporary Redirect\r\n";
+            const char *RETURN_NOT_FOUND = "HTTP/1.1 404 Not Found\r\n";
+        } http;
+        /** structure clientTCP
+        */
+        struct _clientTCP_t {
+            static constexpr int MAX_TCP_CLIENTS = MBED_CONF_LWIP_TCP_SOCKET_MAX - 2; // 1 socket for serverTCP 2 for clientsTCP 1 for temporary socket
+            TCPSocket *socket = nullptr;
+            volatile bool event = true;
+        };
+    private:
+        struct _linkTCP_t {
+            tr_status_t status;
+            bool SET;
+            bool DHCP;
+            bool SERVER;
+            string IP;
+            uint16_t PORT;
+            string MASK;
+            string GATEWAY;
+            uint16_t TIMEOUT;
+            _linkTCP_t():
+                status(RED_DISCONNECTED),
+                SET(false),
+                DHCP(false),
+                SERVER(false),
+                IP("192.168.1.1"),
+                PORT(80),
+                MASK("255.255.255.0"),
+                GATEWAY("192.168.1.0"),
+                TIMEOUT(100)
+            {}
+        };
+
+        struct _clientsTCP_t {
+            volatile int active;
+            volatile int event;
+            _clientTCP_t list[_clientTCP_t::MAX_TCP_CLIENTS];
+            _clientsTCP_t():
+                active(0),
+                event(0)
+            {}
+        };
+
+        _linkTCP_t          _linkTCP;
+        _clientsTCP_t       _clientsTCP;
+        TCPSocket           _serverTCP;
+        EventQueue          _queue;
+        #if MBED_MAJOR_VERSION > 5
+        UnbufferedSerial    *_serial = nullptr;
+        #else
+        Serial              *_serial = nullptr;
+        #endif
+        Thread              *_evenThread = nullptr;
+
+        // Transmission constructor order, don't change
+        USBCDC              *_usb = nullptr;
+        EthernetInterface   *_eth = nullptr;
+        string              (*_processing)(string, _clientTCP_t *);
+        void                (*_ethup)(void);
+        bool                _TermChar = false;
+        bool                _caseIgnore = false;
+
+        void                serial_event(void);
+
+        bool                eth_connect(void);
+        void                eth_event(nsapi_event_t, intptr_t);
+        intptr_t            eth_status(const string&, const intptr_t&);
+        nsapi_error_t       eth_error(const string& SOURCE, const nsapi_error_t& CODE);
+
+        bool                serverTCP_connect(void);
+        void                serverTCP_accept(void);
+        void                serverTCP_close(void);
+
+        void                clientTCP_event(_clientTCP_t &client);
+        void                clientTCP_close(_clientTCP_t &client);
+        
+        void                preprocessing(char *, const tr_delivery_t, _clientTCP_t *client=nullptr);
+    public:
         /** make new Transmission instance
         *
         * @param 
@@ -98,7 +215,7 @@ class Transmission
             #endif
             USBCDC              *usb,
             EthernetInterface   *eth,
-            string              (*processing)(string) = nullptr,
+            string              (*processing)(string, _clientTCP_t *) = nullptr,
             void                (*ethup)(void) = nullptr,
             bool                TermChar = true,
             bool                caseIgnore = true);
@@ -113,7 +230,7 @@ class Transmission
             Serial              *serial,
             #endif
             USBCDC              *usb,
-            string              (*processing)(string) = nullptr,
+            string              (*processing)(string, _clientTCP_t *) = nullptr,
             bool                TermChar = true,
             bool                caseIgnore = true);
         /** make new Transmission instance
@@ -127,7 +244,7 @@ class Transmission
             Serial              *serial,
             #endif
             EthernetInterface   *eth,
-            string              (*processing)(string) = nullptr,
+            string              (*processing)(string, _clientTCP_t *) = nullptr,
             void                (*ethup)(void) = nullptr,
             bool                TermChar = true,
             bool                caseIgnore = true);
@@ -138,7 +255,7 @@ class Transmission
         Transmission(
             USBCDC              *usb,
             EthernetInterface   *eth,
-            string              (*processing)(string) = nullptr,
+            string              (*processing)(string, _clientTCP_t *) = nullptr,
             void                (*ethup)(void) = nullptr,
             bool                TermChar = true,
             bool                caseIgnore = true);
@@ -152,7 +269,7 @@ class Transmission
             #else
             Serial              *serial,
             #endif
-            string              (*processing)(string) = nullptr,
+            string              (*processing)(string, _clientTCP_t *) = nullptr,
             bool                TermChar = true,
             bool                caseIgnore = true);
         /** make new Transmission instance
@@ -161,7 +278,7 @@ class Transmission
         */
         Transmission(
             EthernetInterface   *eth,
-            string              (*processing)(string) = nullptr,
+            string              (*processing)(string, _clientTCP_t *) = nullptr,
             void                (*ethup)(void) = nullptr,
             bool                TermChar = true,
             bool                caseIgnore = true);
@@ -171,7 +288,7 @@ class Transmission
         */
         Transmission(
             USBCDC              *usb,
-            string              (*processing)(string) = nullptr,
+            string              (*processing)(string, _clientTCP_t *) = nullptr,
             bool                TermChar = true,
             bool                caseIgnore = true);
         /** Configure the TCP connection
@@ -179,30 +296,30 @@ class Transmission
         * @param set enable for the tcp connection
         * @returns MAC adress
         */
-        string              ip(const bool set, const char* ip="", const uint16_t port=80, const char* mask="255.255.255.0", const char* gateway="192.168.1.1", const uint16_t timeout=100);
+        string ip(const bool set, const char* ip="", const uint16_t port=80, const char* mask="255.255.255.0", const char* gateway="192.168.1.1", const uint16_t timeout=100);
         /** Return ip config
         *
         * @param ip="" If the specified ip is the one configured gives the complete ip configuration
         * @returns ip config
         */
-        string              ip(string ip="");
+        string ip(string ip="");
         /** Return client config
         *
         * @returns ip:port config
         */
-        string              client(void);
+        string client(void);
         /** scans the reception buffers of transmission TCP and USB
         *
         * @returns enumerator of the different ethernet connexion status of the transmission library
         */
-        enum_trans_status   recv(void);
+        tr_status_t recv(void);
         /** send the buffer to the specified transmission delivery
         *
         * @param ssend sent over transmission
         * @param delivery of the transmission
-        * @returns 
+        * @returns nsapi_error_t code
         */
-        nsapi_error_t       send(const string& ssend, const enum_trans_delivery& delivery=ANY_DELIVERY);
+        nsapi_error_t send(const string& ssend, const tr_delivery_t delivery=ANY_DELIVERY, _clientTCP_t *client=nullptr);
         /** send a request to a server at the specified port
         *
         * @param ssend sent to server
@@ -210,7 +327,7 @@ class Transmission
         * @param port port
         * @returns server response
         */
-        string              get(const string& ssend, const string& server, const int& port=80);
+        string get(const string& ssend, const string& server, const int& port=80);
         /** send an email to an smtp server
         *
         * @param mail is the recipient's email
@@ -220,50 +337,19 @@ class Transmission
         * @param server="129.175.212.70" this is an ip from an smtp server
         * @returns indicates if the smtp transaction was successful
         */
-        bool                smtp(string mail, string from, string subject, string data, const char* server=TRANSMISSION_SMTP_SERVER);
+        bool smtp(string mail, string from, string subject, string data, const char* server=TRANSMISSION_SMTP_SERVER);
         /** ask an smtp server if email exist
         *
         * @param mail is the recipient's email
         * @param server="129.175.212.70" this is an ip from an smtp server
         * @returns indicates if the smtp transaction was successful
         */
-        bool                exist(string mail, const char* server=TRANSMISSION_SMTP_SERVER);
+        bool exist(string mail, const char* server=TRANSMISSION_SMTP_SERVER);
         /** time request to an ntp server
         *
         * @param server="129.175.34.43" this is an ip from an ntp server
         * @returns time
         */
-        time_t              ntp(const char* server=TRANSMISSION_NTP_SERVER);
-
-    private:
-        #if MBED_MAJOR_VERSION > 5
-        UnbufferedSerial    *_serial = nullptr;
-        #else
-        Serial              *_serial = nullptr;
-        #endif
-        TCPSocket           _serverTCP, *_clientTCP = nullptr;
-        Thread              *_evenThread = nullptr;
-        EventQueue          _queue;
-        EthernetInterface   *_eth = nullptr;
-        USBCDC              *_usb = nullptr;
-        bool                _caseIgnore = false;
-        bool                _TermChar = false;
-
-        void                serial_event(void);
-
-        void                eth_state(void);
-        bool                eth_connect(void);
-        void                eth_event(nsapi_event_t, intptr_t);
-        intptr_t            eth_status(const string&, const intptr_t&);
-        nsapi_error_t       eth_error(const string& SOURCE, const nsapi_error_t& CODE);
-
-        bool                serverTCP_connect(void);
-        void                serverTCP_accept(void);
-
-        void                (*_ethup)(void);
-        string              (*_processing)(string);
-        void                preprocessing(char *, const enum_trans_delivery);
-        struct              { enum_trans_status status; bool SET; bool DHCP; bool CONNECT; string IP; uint16_t PORT; string MASK; string GATEWAY; uint16_t TIMEOUT; }
-                            message = { RED_DISCONNECTED, false, false, false, "192.168.1.1", 80, "255.255.255.0", "192.168.1.0", 100 };
+        time_t ntp(const char* server=TRANSMISSION_NTP_SERVER);
 };
 #endif
